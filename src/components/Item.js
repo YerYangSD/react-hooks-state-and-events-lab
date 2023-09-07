53c8c6380c39154385293d90124a3efbe2815e62
Yer Yang <yyhmg_86@yahoo.com>
2023-09-06 22:55:19 -0700

    uses useState hook to add or remove items from cart and toggle cart button color between yellow and magenta

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState(false)
+  const itemClass = cart ? "in-cart" : ""
+  const buttonClass = cart ? "remove" : "add"
+
+  function handleCartClick() {
+    setCart(!cart)
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleCartClick}>{cart ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
